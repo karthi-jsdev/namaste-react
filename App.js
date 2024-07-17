24567c5541e3ad0f2b96fbf69d8d3be7cc6c89ba
karthi-jsdev <karthi.jsdev@gmail.com>
2024-07-17 11:53:38 +0530

    EPISODE-01

---
+++ App.js
@@ -0,0 +1,17 @@
+// const Heading = React.createElement("h1",{id:"heading"},"Hello world from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(Heading);
+
+const Heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "head-tag" }, "I am h1 Tag"),
+    React.createElement("h2", { id: "head-tag" }, "I am h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "head-tag" }, "I am h1 Tag"),
+    React.createElement("h2", { id: "head-tag" }, "I am h2 Tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Heading);

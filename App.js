3c749a07952e307823d15f8d149020d6b38587bf
karthi-jsdev <karthi.jsdev@gmail.com>
2024-10-13 09:27:54 +0530

    initial car component added

--- App.js
+++ App.js
@@ -1,18 +1,65 @@
-// const Heading = React.createElement("h1",{id:"heading"},"Hello world from React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(Heading);
-import React  from "react";
-import ReactDOM  from "react-dom/client";
-const Heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "head-tag" }, "I am h1 Tag"),
-    React.createElement("h2", { id: "head-tag" }, "I am h2 Tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "head-tag" }, "I am h1 Tag"),
-    React.createElement("h2", { id: "head-tag" }, "I am h2 Tag"),
-  ]),
-]);
+/**
+ * Header
+ *  - Logo
+ *  - navcomponent
+ * Body
+ *  - search
+ *  - RestaurantCard
+ *     - Img
+ *      - Name of Res, Star Rating,cuisine,delievery time
+ * Footer
+ *  -copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li> 
+                </ul>
+            </div>
+        </div>
+    )
+}
+const RestaurantCard = () =>{
+    return (
+        <div className="res-card">
+            <img className="res-logo" alt="img-logo" src="https://www.chefkunalkapur.com/wp-content/uploads/2021/03/veg-biryani-scaled.jpeg?v=1618288847" />
+            <h3>Maghana Food</h3>
+            <h4>Biriyani South Indian Asian</h4>
+            <h4>34 mins</h4>
+            <h4>4.4 Ratings</h4>
+        </div>
+    )
+}
+const Body = ()=>{
+    return(
+        <div className="body">
+            <div className="search">search</div>
+            <div className="res-conatiner"><RestaurantCard /></div>
+        </div>
+    )
+}
+
+const AppLayout = () =>{
+    return(<div>
+        <Header/>
+        <Body/>
+    </div>)
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Heading);
+root.render(<AppLayout />);
